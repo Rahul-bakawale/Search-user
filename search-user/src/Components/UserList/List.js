10c749cac33d09b6aabc4653fdbd503a41c5f744
rahul <bakawale51@gmail.com>
2022-09-12 19:05:15 +0530

    Show all User List with MaP

--- search-user/src/Components/UserList/List.js
+++ search-user/src/Components/UserList/List.js
@@ -64,13 +64,39 @@ import { data as useList } from "./UserData";
             </div>
           </main>
     </div> */}
+  <div id="table-wrapper">
+    <div id="table-headers">
+      <table>
+        <thead>
+          <tr>
+            <th className="column1">Id</th>
+            <th className="column2">FirstName</th>
+            <th className="column3">LastName</th>
+            <th className="column4">Email</th>
+            <th className="column5">Phone</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
     {data.map((item,index) => {
-      return(
-        <>
-         <p>{item.id}</p> 
-        </>
-      )
-    })}
+    return(
+    <div id="table-data">
+    <table>
+      <tbody>
+        <tr className="data-row">
+          <td className="column1">{item.id}</td>
+          <td className="column2">{item.firstName}</td>
+          <td className="column3">{item.lastName}</td>
+          <td className="column4">{item.email}</td>
+          <td className="column5">{item.phone}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  )
+  })}
+</div>
+    
     </>
   );
 };
